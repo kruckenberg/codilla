6ae9fe6d66bf259b32874c9f5b2449d1f6803240
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-07-13 09:03:35 -0500

    use md for challenge instruct; add (imperfect) syntax highlighting in instruct

--- code_challenge/views.py
+++ code_challenge/views.py
@@ -3,23 +3,16 @@ import markdown
 
 
 def index(request):
-    instructions_text = """Create a function subtractTwo that accepts number and returns that number minus 2.
-    <br><br>
-    Then create a function <code>map</code> that takes two arguments:
-    <ul>
-      <li>an array of numbers</li>
-      <li>a callback function - this function is applied to each element of the array (from left to right) and the results are stored in a new array</li>
-    </ul>
-    <br><br>
-    Have your <code>map</code> function return a new array filled with numbers that are the result of the callback function on each element of the input array. Please do not use the native <code>map</code> or <code>forEach</code> functions.
-    """
-
+    with open("code_challenge/challenges/map.md", "r", encoding="utf-8") as file:
+        instructions_markdown = file.read()
     starter_code = """function subtractTwo(num) {\n  // your code here\n}\n\nfunction map(arr, callback) {\n  // your code here\n}"""
 
     context = {
         "challenge": {
             "title": "map",
-            "instructions": instructions_text,
+            "instructions": markdown.markdown(
+                instructions_markdown, extensions=["fenced_code", "codehilite"]
+            ),
             "starter_code": starter_code,
         }
     }
