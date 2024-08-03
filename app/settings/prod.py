9e06067652bcd507d2e824208d444dca5291d940
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-08-03 17:03:25 -0500

    change domain, routing in prep for including other homepage content

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -4,10 +4,14 @@ DEBUG = False
 
 ADMINS = [("nkruc", "nkruckenberg@gmail.com")]
 
-ALLOWED_HOSTS = ["codilla.northridge.dev", "localhost"]
+ALLOWED_HOSTS = [
+    "northridge.dev",
+    "www.northridge.dev",
+    "localhost",
+]
 
-CSRF_COOKIE_DOMAIN = "codilla.northridge.dev"
+CSRF_COOKIE_DOMAIN = "northridge.dev"
 CSRF_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ["codilla.northridge.dev"]
+CSRF_TRUSTED_ORIGINS = ["northridge.dev"]
 SESSION_COOKIE_SECURE = True
