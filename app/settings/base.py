a83b45e131304d6f0eeb2018d9f86c8c3248e074
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-08-16 15:18:09 -0500

    fix local file serving

--- app/settings/base.py
+++ app/settings/base.py
@@ -110,13 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
-STATIC_ROOT = "static/"
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 COURSE_ROOT = env("COURSE_ROOT")
--- app/settings/local.py
+++ app/settings/local.py
@@ -2,3 +2,6 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "common/static"]
