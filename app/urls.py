47bf34f8bd2fbabb1797fcc0b4fe2e767e26e3e4
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-08-17 04:26:20 -0500

    redirect to same page on logout

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,16 @@ from django.urls import include, path
 from django.conf import settings
 from django.contrib.staticfiles.views import serve
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+
+
+def custom_logout_view(request):
+    logout(request)
+
+    next_url = request.META.get("HTTP_REFERER", "/")
+
+    return redirect(next_url)
 
 
 def custom_serve(request, path, insecure=False, **kwargs):
@@ -21,4 +31,5 @@ urlpatterns = [
     path("codilla/", include("code_challenge.urls")),
     path("", include("nrp.urls")),
     *static(settings.STATIC_URL, view=custom_serve),
+    path("logout/", custom_logout_view, name="logout"),
 ]
