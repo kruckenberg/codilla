aa4506e6bd249811f83cb323e30eda86b5a45207
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-08-20 21:00:48 -0500

    remove terminal-only method

--- code_challenge/js/webContainer.ts
+++ code_challenge/js/webContainer.ts
@@ -128,33 +128,6 @@ export class CodeContainer {
     }
   }
 
-  async startShell(terminal: Terminal): Promise<WebContainerProcess> {
-    const shellProcess = await this.container.spawn("jsh", {
-      terminal: { cols: terminal.cols, rows: terminal.rows },
-    });
-    shellProcess.output.pipeTo(
-      new WritableStream({
-        write(data) {
-          // Hack to clear shell startup output
-          if (data.includes('Type ".help" for more information')) {
-            terminal.clear();
-            terminal.focus();
-            return;
-          }
-          terminal.write(data);
-        },
-      }),
-    );
-    const input = shellProcess.input.getWriter();
-    terminal.onData((data: string) => {
-      input.write(data);
-    });
-
-    terminal.input("node\n", false);
-
-    return shellProcess;
-  }
-
   async test() {
     this.clearOutput();
 
